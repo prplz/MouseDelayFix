dff0cecae5c004ad211cba697a9e5ae9704ae8cd
Michael Himing <mhiming@gmail.com>
2017-05-01 21:24:24 +1000

    Release 1.0

--- src/main/java/io/prplz/mousedelayfix/MouseDelayFix.java
+++ src/main/java/io/prplz/mousedelayfix/MouseDelayFix.java
@@ -11,7 +11,7 @@ public class MouseDelayFix extends DummyModContainer {
 
     public static final String MOD_ID = "mousedelayfix";
     public static final String MOD_NAME = "MouseDelayFix";
-    public static final String VERSION = "0.1-SNAPSHOT";
+    public static final String VERSION = "1.0";
 
     public MouseDelayFix() {
         super(new ModMetadata());
